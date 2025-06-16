6661047e9adf28da4cad5db75daf4e9e0bd4fd2b
ABDUL SAMIM MONDAL <mercuryman912@gmail.com>
2025-06-16 17:16:49 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-DB_USER = os.getenv("DB_USER", "admin")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "supersecure")
-DB_HOST = os.getenv("DB_HOST", "your-rds-host.amazonaws.com")
-DB_NAME = os.getenv("DB_NAME", "workoutdb")
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
